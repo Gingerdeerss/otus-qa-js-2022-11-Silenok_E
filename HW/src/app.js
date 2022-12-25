cdd9b6c3057bc33cf97831549d8cd77d261190c2
Elena Silenok <silenok.e@ati.su>
2022-12-25 16:31:45 +0300

    Домашнее задание по объектам

---
+++ HW/src/app.js
@@ -0,0 +1,16 @@
+
+const scores = {
+    Anna: 10,
+    Olga: 1,
+    Ivan: 5,
+    };
+    const getScore = (scores) => {
+    let sum = 0;
+    for (let key in scores) { 
+        sum += scores[key];
+} 
+    return sum;
+    }
+    console.log(getScore(scores));
+
+  
